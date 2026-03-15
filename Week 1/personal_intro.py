0ee455076851c7a147588187ae2d9f45392e92e2
Diptesh Dange <dipteshdange@gmail.com>
2026-03-15 20:26:37 +0530

    Add files via upload

---
+++ Week 1/personal_intro.py
@@ -0,0 +1,10 @@
+
+print("Welcome to the Personal Introduction Program!\n")
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+
+print("\nNice to meet you,", name + "!")
+print(f"You are {age} years old and your favorite hobby is {hobby}.")
+print("Thanks for sharing! ")
